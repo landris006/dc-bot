675ac7b49afa8cbb25ff490a74d8b19c801f7d7f
landris006 <laczko.andris007@gmail.com>
2023-01-16 12:08:32 +0100

    unnecessary member update

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -1,37 +1,10 @@
 import { Message } from 'discord.js';
-import { upsertMember } from '../utils/upsertMember';
 import fs from 'fs';
 
 export const messageHandler = async (message: Message) => {
   if (message.member?.user.bot) {
     return;
   }
-
-  const guildID = message.guild?.id;
-  const member = message.member;
-
-  if (!member || !guildID) {
-    return;
-  }
-
-  const username = member.user.username;
-  const userID = member.id;
-
-  await upsertMember(
-    {
-      userID,
-      username,
-      guildID,
-      nickname: member.nickname,
-      joinedAt: member.joinedAt,
-      avatarURL: member.user.avatarURL(),
-    },
-    {
-      messagesSent: {
-        increment: 1,
-      },
-    }
-  );
 
   if (message.content.includes('yorokobe shounen')) {
     fs.readdir('./assets/kirei/', (err, files) => {
