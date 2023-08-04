f6831621e54255cfb8e485351908efd81c289334
landris006 <laczko.andris007@gmail.com>
2023-08-04 22:19:20 +0200

    changed default port

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ export interface ClientState {
   isPlayingMinecraft: string | null;
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 export const io = new Server(+PORT, {
   cors: {
     origin: process.env.CORS_URL,
